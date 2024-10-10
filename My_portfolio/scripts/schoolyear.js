9fb4dc20decc8380925ac0fb1440918d3becc52e
Jakub Holík <holikj1@gmail.com>
2024-10-11 01:15:41 +0200

    some minor changes

--- My_portfolio/scripts/schoolyear.js
+++ My_portfolio/scripts/schoolyear.js
@@ -1,15 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
     const startDate = new Date('September 2, 2024');
-    const endDate = new Date(startDate);
-    endDate.setDate(startDate.getDate() + 36 * 7); 
+    const endDate = new Date("June 30, 2025");
 
     const updateProgressBar = () => {
         const now = new Date();
+        console.log(now);
         const totalDuration = endDate - startDate;
         const timeElapsed = now - startDate;
         const progressPercentage = Math.min((timeElapsed / totalDuration) * 100, 100);
 
         document.getElementById('progressIndicator').style.width = `${progressPercentage}%`;
+        console.log(progressPercentage);
     };
 
     updateProgressBar();
